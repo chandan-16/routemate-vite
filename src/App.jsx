3c3876df88e36e80dfcba1aebb431d84734a6bbf
chandan-16 <chandangautam420.cg@gmail.com>
2026-06-27 14:08:33 +0530

    Building a routemate app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
